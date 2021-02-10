5137dc6c2bc34dc66a4837c8f7899b8a368e2e7f
Caleb Owens <owenscaleb75@gmail.com>
2021-02-10 00:57:37 -0600

    working with properties objects and access

---
+++ classCsharp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace classTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var Burglar = new test1("a1z5", "Steven", 1000);
+            var Nick = new test1("tk421", "Nick", 100000);
+
+            Console.WriteLine(Nick.money);
+            Burglar.drainAccount(Nick);
+
+            //Console.WriteLine(Nick.money);
+
+
+
+
+        }
+    }
+}
---
+++ classCsharp/test1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+
+  public class test1
+  {
+    public string name { get; set; }
+    private string Id { get; set; }
+    public decimal money { get; set; }
+
+    public test1(string name, string id, decimal money){
+        name = this.name;
+        id = this.id;
+        money = this.money;
+    }
+  
+
+    public void drainAccount(test1 person)
+    {
+        if(person.name == "Nick")
+        {
+            person.money = 0;
+
+        }
+    }
+}
