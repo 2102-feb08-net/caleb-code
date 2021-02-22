df2c781b666db2f2cf87b4b8e8057d71adcc2703
Caleb Owens <owenscaleb75@gmail.com>
2021-02-22 11:25:44 -0600

    more updates to SQL assignment1

--- SQLInClassAssignment1.sql
+++ SQLInClassAssignment1.sql
@@ -6,15 +6,46 @@ LastName NVARCHAR(100),
 Cardnumber DECIMAL 
 );
 
+CREATE TABLE Products(
+	ID INT NOT NULL PRIMARY KEY,
+	Name NVARCHAR(200),
+	Price MONEY
+);
+
 CREATE TABLE Orders(
 
 	ID INT NOT NULL,
 	ProductID INT,
 	CustomerID INT
+	FOREIGN KEY(CustomerID) REFERENCES Customers(ID),
+	FOREIGN KEY(ProductID) REFERENCES Products(ID)
 );
 
-CREATE TABLE Products(
-	ID INT NOT NULL,
-	Name NVARCHAR(200),
-	Price MONEY
-);
+INSERT INTO Customers(ID, FirstName, LastName, Cardnumber) VALUES
+	(0, 'Jebediah', 'Georgie', 457239),
+	(1, 'Sara', 'Miller', 492384),
+	(2, 'Brock', 'TheRock', 123456)
+
+INSERT INTO Products(ID, Name, Price) VALUES
+	(0, 'Apple', 1.00),
+	(1, 'Orange', 2.00),
+	(2, 'Banana', 3.00)
+
+INSERT INTO Orders(ID, ProductID, CustomerID) VALUES
+	(0, 0, 0),
+	(1, 1, 1),
+	(2, 2, 2)
+
+INSERT INTO Products(ID, Name, Price) VALUES
+	(3, 'Iphone', 200.00)
+
+INSERT INTO Customers(ID, FirstName, LastName, Cardnumber) VALUES
+	(3, 'Tina', 'Smith', 987654)
+
+INSERT INTO Orders(ID, ProductID, CustomerID) VALUES
+	(3, 3, 3)
+
+
+SELECT * FROM Customers
+WHERE Customers.FirstName = 'Tina'
+AND Customers.LastName = 'Smith'
